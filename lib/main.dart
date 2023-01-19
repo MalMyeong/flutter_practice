7d6d071b89f9845a0d10be60ab2b89a1730b51f3
MalMyeong <audgus4321@naver.com>
2023-01-19 23:22:24 +0900

    Add button('Request')

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String? name;
-
-  Player();
-}
-
-void main() {
-  var nico = Player();
-  runApp(App());
-}
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,7 +8,7 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -76,6 +65,7 @@ class App extends StatelessWidget {
                 height: 30,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
                     decoration: BoxDecoration(
@@ -88,6 +78,23 @@ class App extends StatelessWidget {
                       child: Text(
                         'Transfer',
                         style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1F2123),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                      child: Text(
+                        'Request',
+                        style: TextStyle(
+                          color: Colors.white,
                           fontSize: 20,
                         ),
                       ),
